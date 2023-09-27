0c6e8631b7f3bfc7389dcd1c463d3d7319a24fd7
Vivizera <vitoriabachmann2006@gmail.com>
2023-09-27 16:38:28 -0300

    Alteracao estrutura codigo

---
+++ CardapioPadaria/src/ifsc/Produto.java
@@ -0,0 +1,41 @@
+package ifsc;
+
+public class Produto {
+	private String nome;
+	private String sabor;
+	private String descricao;
+	private Float preco;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+
+}
--- CardapioPadaria/src/ifsc/SalgadoMain.java
+++ CardapioPadaria/src/ifsc/SalgadoMain.java
@@ -6,21 +6,25 @@ public class SalgadoMain {
 		// TODO Auto-generated method stub
         Salgado coxinha = new Salgado();
         coxinha.setNome("Coxinha de Frango");
+        coxinha.setSabor("Frango");
         coxinha.setDescricao("");
         coxinha.setPreco(5.00f);
         
         Salgado bolinho = new Salgado();
         bolinho.setNome("Bolinho de Carne");
+        bolinho.setSabor("Carne");
         bolinho.setDescricao("");
         bolinho.setPreco(4.00f);
         
         Salgado pastel1 = new Salgado();
         pastel1.setNome("Pastel de Frango");
+        pastel1.setSabor("Frango");
         pastel1.setDescricao("");
         pastel1.setPreco(6.00f);
         
         Salgado pastel2 = new Salgado();
         pastel2.setNome("Pastel de Carne");
+        pastel2.setSabor("Carne");
         pastel2.setDescricao("");
         pastel2.setPreco(5.00f);
 	}
